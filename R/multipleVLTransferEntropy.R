41ecaddb9d66936292609cfb728e231bb4ca564d
Chainarong Amornbunchornvej <grandca@gmail.com>
2019-12-02 13:42:44 +0700

    Update Transfer Entropy
    Reduced redundancy

--- R/multipleVLTransferEntropy.R
+++ R/multipleVLTransferEntropy.R
@@ -10,9 +10,18 @@ multipleVLTransferEntropy<-function(TS,maxLag,nboot=0,lx=1,ly=1,VLflag=TRUE,auto
     for(j in seq(i+1,m))
     {
       outij<-VLTransferEntropy(Y=TS[,j],X=TS[,i], maxLag=maxLag,nboot=nboot,lx=lx,ly=ly,VLflag=VLflag,autoLagflag=autoLagflag)
-      outji<-VLTransferEntropy(Y=TS[,i],X=TS[,j], maxLag=maxLag,nboot=nboot,lx=lx,ly=ly,VLflag=VLflag,autoLagflag=autoLagflag)
-      adjMat[i,j]<-outij$XgCsY_trns
-      adjMat[j,i]<-outji$XgCsY_trns
+      if(outij$XgCsY_trns)
+      {
+        adjMat[i,j]<-outij$XgCsY_trns
+        adjMat[j,i]<-FALSE
+      }else
+      {
+        adjMat[i,j]<-FALSE
+        TEratio<-1/outij$TEratio
+        if(!is.na(TEratio))
+          if( TEratio >1)
+            adjMat[j,i]<-TRUE
+      }
     }
   res<-list(adjMat=adjMat)
   return(res)
